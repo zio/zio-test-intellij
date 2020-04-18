0766b42c2e3912e26817e07ea40840076c13fe5e
Mitsutaka Takeda <mitsutaka.takeda@gmail.com>
2020-04-18 21:13:56 +0900

    Read arguments from a file if command line shortner is enabled (#1)
    When command line arguments for a test runner is too long, they are passed via a temporary file and its path is passed via the command line argument. This commit reads command line arguments from a file if necessary.

---
+++ src/shared/test/scala/zio/intellij/testsupport/ZTestRunnerSpec.scala
@@ -0,0 +1,37 @@
+package zio.intellij.testsupport
+
+import java.io.File
+
+import zio.test.Assertion._
+import zio.test._
+import zio.{Has, UIO, ZIO, ZLayer, ZManaged}
+
+object ZTestRunnerSpec extends DefaultRunnableSpec {
+
+  private def tempArgFile: ZLayer[Any, Throwable, Has[File]] = ZManaged.makeEffect {
+    val tmpFile = File.createTempFile("ZTestRunnerSpec-", ".tmp")
+    val writer = new java.io.PrintWriter(tmpFile)
+    try {
+      writer.print(
+        s"""-testClassTerm
+           |class
+           |-testMethodTerm
+           |method
+           |""".stripMargin
+      )
+    } finally writer.close()
+    tmpFile
+  } { tmpFile => tmpFile.delete() }.toLayer
+
+  override def spec: ZSpec[zio.test.environment.TestEnvironment, Any] = suite("ZTestRunnerSpec")(
+    testM("read arguments from file if command line argument is a file path") {
+      (for {
+        argFile <- ZIO.access[Has[File]](_.get)
+        args <- UIO(ZTestRunner.Args.readArgsFromFileIfCommandLineShortenerIsEnabled(Array(s"@${argFile.getAbsolutePath}")).get)
+      } yield assert(args.toList)(hasSize(equalTo(4)))).provideLayer(tempArgFile)
+    },
+    test("ignore non-file command line argument"){
+      assert(ZTestRunner.Args.readArgsFromFileIfCommandLineShortenerIsEnabled(Array("abc", "efg")))(isNone)
+    }
+  )
+}
