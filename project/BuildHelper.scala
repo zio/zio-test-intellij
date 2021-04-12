ec0c6e2556163eb520d9c8431e0a4c10846b84c0
Alpha <666504+alphaho@users.noreply.github.com>
2021-04-12 14:25:32 +0800

    Update scala 3 to 3.0.0-RC2

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -28,7 +28,7 @@ object BuildHelper {
   final private val Scala211 = "2.11.12"
   final private val Scala212 = "2.12.13"
   final private val Scala213 = "2.13.5"
-  final private val Scala3   = "3.0.0-RC1"
+  final private val Scala3   = "3.0.0-RC2"
 
   final val scala3Settings = Seq(
     crossScalaVersions += Scala3
