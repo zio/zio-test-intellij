c1e86b2313b908de254783c29e20d8afc11ca05d
Igal Tabachnik <hmemcpy@gmail.com>
2021-04-24 00:08:07 +0300

    Building for 1.0.7

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -28,7 +28,7 @@ object BuildHelper {
   final private val Scala211 = "2.11.12"
   final private val Scala212 = "2.12.13"
   final private val Scala213 = "2.13.5"
-  final private val Scala3   = "3.0.0-RC2"
+  final private val Scala3   = "3.0.0-RC3"
 
   final val scala3Settings = Seq(
     crossScalaVersions += Scala3
