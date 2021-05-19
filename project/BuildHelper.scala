20b42315e01b1cea61eac627aff5f4e437a62692
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-05-19 09:48:23 +0200

    Update scala-library to 2.13.6 (#79)
    Co-authored-by: Igal Tabachnik <hmemcpy@gmail.com>

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -14,7 +14,7 @@ object BuildHelper {
 
   final private val Scala211 = "2.11.12"
   final private val Scala212 = "2.12.13"
-  final private val Scala213 = "2.13.5"
+  final private val Scala213 = "2.13.6"
   final private val Scala3   = "3.0.0"
 
   final val scala3Settings = Seq(
