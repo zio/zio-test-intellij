ed0ecc6be57bbd7527e25d2037cc4c5811a0f58c
Scala Steward <scala_steward@virtuslab.com>
2023-06-13 02:58:36 +0000

    Update scala-library to 2.13.11

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -14,7 +14,7 @@ object BuildHelper {
 
   final private val Scala211 = "2.11.12"
   final private val Scala212 = "2.12.15"
-  final private val Scala213 = "2.13.8"
+  final private val Scala213 = "2.13.11"
   final private val Scala3   = "3.2.1"
 
   final val scala3Settings = Seq(
